05bd9bb9a74c9c06dc9295711c9b5eebd7e3b4c5
Mario Korzun <rkorzun2020@gmail.com>
2026-04-14 15:57:36 +0400

    Add tasks for Homework 7 with initial scripts and comments

---
+++ js/task-1.js
@@ -0,0 +1,38 @@
+// Завдання 1
+// HTML містить список категорій ul#categories.
+// <ul id="categories">
+//   <li class="item">
+//     <h2>Animals</h2>
+//     <ul>
+//       <li>Cat</li>
+//       <li>Hamster</li>
+//       <li>Horse</li>
+//       <li>Parrot</li>
+//     </ul>
+//   </li>
+//   <li class="item">
+//     <h2>Products</h2>
+//     <ul>
+//       <li>Bread</li>
+//       <li>Parsley</li>
+//       <li>Cheese</li>
+//     </ul>
+//   </li>
+//   <li class="item">
+//     <h2>Technologies</h2>
+//     <ul>
+//       <li>HTML</li>
+//       <li>CSS</li>
+//       <li>JavaScript</li>
+//       <li>React</li>
+//       <li>Node.js</li>
+//     </ul>
+//   </li>
+// </ul>
+
+// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
+
+// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного елемента li.item у списку ul#categories
+// знайде й виведе в консоль текст заголовка елемента (тегу <h2>)
+// і кількість елементів у категорії (усіх <li>, вкладених у нього).
